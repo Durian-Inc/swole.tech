6d74e2a9cd111da454fac3936c110477b47634d5
Clay McGinnis <mcginnis@claymav.com>
2019-03-01 20:36:43 -0600

    Added components folder

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Main } from './components/index';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,20 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Main />
       </div>
     );
   }
