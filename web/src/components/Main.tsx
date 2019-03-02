2f91c94f873d125d20c58797b00d3cad22364754
Clay McGinnis <mcginnis@claymav.com>
2019-03-01 20:36:34 -0600

    Added components folder

---
+++ web/src/components/Main.tsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+    );
+  }
+}
+
+export default Main;
