d8a87d49aaddf03e2dea52cde3fb022f335a3d81
Clay McGinnis <mcginnis@claymav.com>
2019-03-03 09:48:07 -0600

    Updated address

--- api/run.py
+++ api/run.py
@@ -1,6 +1,6 @@
 """Runs the server"""
-from serve import app
 import controllers
+from serve import app
 
 if __name__ == '__main__':
-    app.run(debug=True, host='localhost', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8000)
